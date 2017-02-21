8ffefd058dcb514d754d9cf139039fa47f202116
Justin Grasse <just.grasse@gmail.com>
2017-02-20 17:48:28 -0800

    create click listener for changing boardSate

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ class Square extends React.Component {
   };
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'J'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
@@ -32,7 +32,7 @@ class Board extends React.Component {
     }
   }
   renderSquare(i) {
-    return <Square value={this.state.squares[i]}/>;
+    return <Square value={this.state.squares[i]} onClick{() => this.handleClick(i)} />;
   }
   render() {
     const status = 'Next player: X';
